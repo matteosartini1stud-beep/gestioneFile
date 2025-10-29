918a0c7f40c4f0b7d104764cb8304d8ac4f2a174
Matteo Sartini <MATTEO.SARTINI@local.ittcesena.it>
2025-10-29 08:51:27 +0100

    R0.1:utilizzo classe writer sul main

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
-﻿using System;
+﻿using gestioneFile;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace gestioneFile
+namespace gestionefile
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Inserisci il path");
+            string path = Console.ReadLine();
+
+            Writer w = new Writer(path);
+
+            w.ScriviFile();
+
+            Console.WriteLine("operazione completata");
         }
     }
 }
