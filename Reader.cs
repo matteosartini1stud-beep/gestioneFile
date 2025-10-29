0bfbbc289147d0cddd0fcf6a7bfa95a6d5f43349
Matteo Sartini <MATTEO.SARTINI@local.ittcesena.it>
2025-10-29 08:48:28 +0100

    R0.1:completata versione reader

--- Reader.cs
+++ Reader.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace gestioneFile
+namespace gestionefile
 {
     internal class Reader
     {
+        private string FilePath;
+
+        public Reader(string Path)
+        {
+            FilePath = Path;
+        }
+        public void LeggiFile()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(FilePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Errore nella lettura del file" + e.Message);
+            }
+        }
     }
 }
